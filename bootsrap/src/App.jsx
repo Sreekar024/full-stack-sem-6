b59beeac41b1f0383b5ea7c135526336e8dd2019
Chimbili Sreekar <23BAI70257@cuchd.in>
2026-02-03 15:24:57 +0530

    ready to deploy

--- bootsrap/src/App.jsx
+++ bootsrap/src/App.jsx
@@ -1,23 +1,9 @@
-import BasicExample from "./components/dropdown";
-import BasicNavbar from "./components/navbar";
-import HeroSection from "./components/hero";
-import CardsSection from "./components/cards";
+import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
 
 function App() {
-  return (
-    <div>
-      <BasicNavbar />
-      <HeroSection />
-      <CardsSection />
-      <div className="gradient-bg py-5">
-        <div className="container">
-          <BasicExample />
-        </div>
-      </div>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ bootsrap/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import BasicExample from "./components/dropdown";
+import BasicNavbar from "./components/navbar";
+import HeroSection from "./components/hero";
+import CardsSection from "./components/cards";
+
+function Home() {
+  return (
+    <div>
+      <BasicNavbar />
+      <HeroSection />
+      <CardsSection />
+      <div className="gradient-bg py-5">
+        <div className="container">
+          <BasicExample />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
